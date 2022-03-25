ff5af2d57125b49e9b3de436e9dc6737eaa4c753
fullstackgjj <fullstackgjj@gmail.com>
2022-03-25 02:42:32 -0400

    Init Commit

---
+++ repl-imports.scm
@@ -0,0 +1,12 @@
+(module repl-imports
+
+(repl-imports)
+
+(import chicken.string srfi-113 srfi-128)
+
+(define (raw-current-import-list) (map (lambda (x) (car (string-split (->string x) "#"))) (map cdr (##sys#current-environment))))
+(define (current-import-set) (list->set (make-default-comparator) (raw-current-import-list)))
+(define (sorted-current-import-list) (sort (set->list (current-import-set)) (lambda (a b) (> 0 (string-compare3 a b)))))
+(define (repl-imports) (sorted-current-import-list))
+
+) ;; end module
