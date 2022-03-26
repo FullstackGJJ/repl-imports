5336000780fdaa8deebd5f844f11582dc7e9e024
fullstackgjj <fullstackgjj@gmail.com>
2022-03-26 05:30:00 -0400

    refactor

--- repl-imports.scm
+++ repl-imports.scm
@@ -4,9 +4,9 @@
 
 (import scheme (chicken sort) (chicken string) srfi-113 srfi-128)
 
-(define (raw-current-import-list) (map (lambda (x) (car (string-split (->string x) "#"))) (map cdr (##sys#current-environment))))
-(define (current-import-set) (list->set (make-default-comparator) (raw-current-import-list)))
-(define (sorted-current-import-list) (sort (set->list (current-import-set)) (lambda (a b) (> 0 (string-compare3 a b)))))
-(define (repl-imports) (sorted-current-import-list))
+(define raw-current-import-list (map (lambda (x) (car (string-split (->string x) "#"))) (map cdr (##sys#current-environment))))
+(define current-import-set (list->set (make-default-comparator) raw-current-import-list))
+(define sorted-current-import-list (sort (set->list current-import-set) (lambda (a b) (> 0 (string-compare3 a b)))))
+(define (repl-imports) sorted-current-import-list)
 
 ) ;; end module
