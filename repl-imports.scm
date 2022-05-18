7d7054907dd8147faaa32a6050e1a09c4771f0de
fullstackgjj <fullstackgjj@gmail.com>
2022-05-18 15:57:01 -0400

    updated to allow excluding

--- repl-imports.scm
+++ repl-imports.scm
@@ -2,11 +2,13 @@
 
 (repl-imports)
 
-(import scheme (chicken sort) (chicken string) srfi-113 srfi-128)
+(import scheme (chicken sort) (chicken string) srfi-1 srfi-113 srfi-128)
 
 (define (raw-current-import-list) (map (lambda (x) (car (string-split (->string x) "#"))) (map cdr (##sys#current-environment))))
 (define (current-import-set) (list->set (make-default-comparator) (raw-current-import-list)))
 (define (sorted-current-import-list) (sort (set->list (current-import-set)) (lambda (a b) (> 0 (string-compare3 a b)))))
-(define (repl-imports) (sorted-current-import-list))
+(define (repl-imports #!optional (exclude-values '()))
+  (lset-difference (lambda (a b) (= (string-compare3 a b) 0))
+                   (sorted-current-import-list) exclude-values))
 
 ) ;; end module
